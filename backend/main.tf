cc91e456da539e1e6b5ab1444352ed1d627080d4
Tommelskinke <eliasherigstad@gmail.com>
2024-10-16 16:13:39 +0200

    main

--- backend/main.tf
+++ backend/main.tf
@@ -1,1 +1,94 @@
-//hello world!
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.4.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-backend-tfstate-esh"    # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "sabetfs0h22mzsx99"         # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "tfstate"                   # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "backend.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+
+  }
+}
+
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+  subscription_id = "683886f5-1985-4666-a868-6e00a44de5bd"
+  client_id       = "6b067221-787a-4af1-9db4-23b857f8be83"
+  client_secret   = "k.t8Q~K5PUrDx~wuN5RHbFoOfdmk777o4B1naa~s"
+  tenant_id       = "dd606718-fb9d-4d8c-b04b-9b5143d2976c"
+}
+
+resource "random_string" "random_string" {
+  length  = 10
+  special = false
+  upper   = false
+}
+
+resource "azurerm_resource_group" "rg_backend" {
+  name     = var.rg_backend_name
+  location = var.rg_backend_location
+}
+
+resource "azurerm_storage_account" "sa_backend" {
+  name                     = "${var.sa_backend_name}${random_string.random_string.result}"
+  resource_group_name      = azurerm_resource_group.rg_backend.name
+  location                 = azurerm_resource_group.rg_backend.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
+resource "azurerm_storage_container" "sc_backend" {
+  name                  = var.sc_backend_name
+  storage_account_name  = azurerm_storage_account.sa_backend.name
+  container_access_type = "private"
+}
+
+
+//Key vault under
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "kv_backend" {
+  name                        = "${(lower(var.kv_backend_name))}${random_string.random_string.result}"
+  location                    = azurerm_resource_group.rg_backend.location
+  resource_group_name         = azurerm_resource_group.rg_backend.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get", "List", "Create",
+    ]
+
+    secret_permissions = [
+      "Get", "List", "Set",
+    ]
+
+    storage_permissions = [
+      "Get", "List", "Set",
+    ]
+  }
+}
+
+resource "azurerm_key_vault_secret" "sa_backend_accesskey" {
+  name         = var.sa_backend_accesskey_name
+  value        = azurerm_storage_account.sa_backend.primary_access_key
+  key_vault_id = azurerm_key_vault.kv_backend.id
+}
+
